642a3881510ecaa556deff2974de65a97f9db5b5
jairoalzate98 <jairo.alzate@uptc.edu.co>
2018-05-09 21:55:07 -0500

    Se redimenciono las imagenes

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -1,6 +1,7 @@
 package views;
 
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,7 +27,10 @@ public class MainWindow extends JFrame{
 		setLayout(new GridLayout(imgList.size()/3, 3));
 		for (Img img : imgList) {
 			try {
-				add(new JLabel(new ImageIcon(new URL(img.getPath()))));
+				JLabel label = new JLabel();
+				ImageIcon imageIcon = new ImageIcon(new ImageIcon(new URL(img.getPath())).getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
+				label.setIcon(imageIcon);
+				add(label);
 			} catch (MalformedURLException e) {
 				System.out.println(e.getMessage());
 			}
